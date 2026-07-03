e8bd3ade72eafc3c7763fd1eac60c3f311608276
mohamedmaghzaoui <mohamedmaghzaoui53@gmail.com>
2026-07-03 15:19:18 +0200

    tech: implement swagger

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express';
 import cors from 'cors';
+
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from './swagger/swagger';
+
 import movieRoutes from './modules/movie/MovieRoutes';
 import animeRoutes from './modules/anime/AnimeRoutes';
 import seriesRoutes from './modules/series/SeriesRoutes';
@@ -8,6 +12,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+//swagger docs
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use('/movies', movieRoutes);
 app.use('/anime', animeRoutes);
 app.use('/series', seriesRoutes);
