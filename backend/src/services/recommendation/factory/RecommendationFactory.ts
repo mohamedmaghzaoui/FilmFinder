aa832f6dbc95f9c97923bc2bbf6defc8c512cb39
mohamedmaghzaoui <mohamedmaghzaoui53@gmail.com>
2026-07-22 09:50:27 +0200

    feat: add recomondation service factory

---
+++ backend/src/services/recommendation/factory/RecommendationFactory.ts
@@ -0,0 +1,55 @@
+import { RecommendationService } from '../RecommendationService';
+
+import { DefaultFilter } from '../filter/DefaultFilter';
+
+import { MovieBarycenterStrategy } from '../scoring/MovieBarycenterStrategy';
+import { AnimeBarycenterStrategy } from '../scoring/AnimeBarycenterStrategy';
+import { SeriesBarycenterStrategy } from '../scoring/SeriesBarycenterStrategy';
+
+import { RatingSort } from '../sorting/RatingSort';
+import { ReleaseYearSort } from '../sorting/ReleaseYearSort';
+import { ScoreSort } from '../sorting/ScoreSort';
+
+import { LoggingScoringDecorator } from '../decorator/LoggingScoringDecorator';
+
+export class RecommendationFactory {
+  static create(type: string, repository: any) {
+    const filterStrategy = new DefaultFilter();
+
+    let scoringStrategy;
+
+    switch (type) {
+      case 'movie':
+        scoringStrategy = new MovieBarycenterStrategy();
+        break;
+
+      case 'anime':
+        scoringStrategy = new AnimeBarycenterStrategy();
+        break;
+
+      case 'series':
+        scoringStrategy = new SeriesBarycenterStrategy();
+        break;
+
+      default:
+        throw new Error('Invalid recommendation type');
+    }
+
+    const decoratedScoringStrategy = new LoggingScoringDecorator(
+      scoringStrategy,
+    );
+
+    const sortStrategies = {
+      rating: new RatingSort(),
+      releaseYear: new ReleaseYearSort(),
+      score: new ScoreSort(),
+    };
+
+    return new RecommendationService(
+      repository,
+      filterStrategy,
+      decoratedScoringStrategy,
+      sortStrategies,
+    );
+  }
+}
